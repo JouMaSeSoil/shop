b4745a7bc1073a16aaa07b75ef5c006c1b24e9b9
Jono Prest <jjprest@gmail.com>
2021-01-08 14:03:12 +0200

    cleaned up a few things

--- src/Components/Navigation/Navigation.js
+++ src/Components/Navigation/Navigation.js
@@ -1,80 +1,90 @@
-import React from 'react';
-import './Navigation.css';
-import { Link, animateScroll as scroll } from "react-scroll";
-import Logo from './logo.jpg';
+import React from "react";
+import "./Navigation.css";
+import { Link } from "react-scroll";
 
+const Navigation = ({ onRouteChange, route }) => {
+  return (
+    <nav className="dt w-100 center nav">
+      <div className="dtc w2 v-mid pa3">
+        <Link
+          activeClass="active"
+          to="Welcome"
+          spy={true}
+          smooth={true}
+          offset={-70}
+          duration={500}
+          className="dib w2 h2 pa1 ba b--white-90 grow-large border-box pointer logo"
+        >
+          <div className="link white-90 hover-white logo"></div>
+        </Link>
+      </div>
 
-
-const Navigation = ({onRouteChange, route}) => {
-
-    return (
-
-        <nav className="dt w-100 center nav">
-            <div className="dtc w2 v-mid pa3">
-
-                <Link activeClass="active"
-                    to='Welcome'
-                    spy={true}
-                    smooth={true}
-                    offset={-70}
-                    duration={500}
-                    className="dib w2 h2 pa1 ba b--white-90 grow-large border-box pointer logo" >
-
-                    <div className="link white-90 hover-white logo"></div>
-                </Link>
-            </div>
-
-            {route==='home'
-            ?   <div className="dtc v-mid tr pa3">
-                <Link activeClass="active"
-                    to='About'
-                    spy={true}
-                    smooth={true}
-                    offset={-70}
-                    duration={500}
-                    className="f6 fw4 hover-white no-underline white-70 dn dib-ns pv2 ph3 pointer" >
-                    About us
-                </Link>
-                <Link activeClass="active"
-                    to='Results'
-                    spy={true}
-                    smooth={true}
-                    offset={-70}
-                    duration={500}
-                    className="f6 fw4 hover-white no-underline white-70 dn dib-ns pv2 ph3" href="/" >Some Results</Link>
-                <Link activeClass="active"
-                    to='Contacts'
-                    spy={true}
-                    smooth={true}
-                    offset={-70}
-                    duration={500}
-                    className="f6 fw4 hover-white no-underline white-70 dn dib-l pv2 ph3" href="/" >Contact</Link>
-                <Link activeClass="active"
-                    to='Product'
-                    spy={true}
-                    smooth={true}
-                    offset={-70}
-                    duration={500}
-                    className="f6 fw4 hover-white no-underline white-70 dib ml2 pv2 ph3 ba pointer" >
-                    Shop
-                </Link>
-            </div>
-            :<div className="dtc v-mid tr pa3" >
-                <Link activeClass="active"
-                    to='Product'
-                    spy={true}
-                    smooth={true}
-                    offset={-70}
-                    duration={500}
-                    className="f6 fw4 hover-white no-underline white-70 dib ml2 pv2 ph3 ba pointer" 
-                    onClick = {()=>onRouteChange('home')}>
-                    Back to shop
-                </Link>
-            </div>
-            }
-        </nav>
-    )
-
-
-}
+      {route === "home" ? (
+        <div className="dtc v-mid tr pa3">
+          <Link
+            activeClass="active"
+            to="About"
+            spy={true}
+            smooth={true}
+            offset={-70}
+            duration={500}
+            className="f6 fw4 hover-white no-underline white-70 dn dib-ns pv2 ph3 pointer"
+          >
+            About us
+          </Link>
+          <Link
+            activeClass="active"
+            to="Results"
+            spy={true}
+            smooth={true}
+            offset={-70}
+            duration={500}
+            className="f6 fw4 hover-white no-underline white-70 dn dib-ns pv2 ph3"
+            href="/"
+          >
+            Some Results
+          </Link>
+          <Link
+            activeClass="active"
+            to="Contacts"
+            spy={true}
+            smooth={true}
+            offset={-70}
+            duration={500}
+            className="f6 fw4 hover-white no-underline white-70 dn dib-l pv2 ph3"
+            href="/"
+          >
+            Contact
+          </Link>
+          <Link
+            activeClass="active"
+            to="Product"
+            spy={true}
+            smooth={true}
+            offset={-70}
+            duration={500}
+            className="f6 fw4 hover-white no-underline white-70 dib ml2 pv2 ph3 ba pointer"
+          >
+            Shop
+          </Link>
+        </div>
+      ) : (
+        <div className="dtc v-mid tr pa3">
+          <Link
+            activeClass="active"
+            to="Product"
+            spy={true}
+            smooth={true}
+            offset={-70}
+            duration={500}
+            className="f6 fw4 hover-white no-underline white-70 dib ml2 pv2 ph3 ba pointer"
+            onClick={() => onRouteChange("home")}
+          >
+            Back to shop
+          </Link>
+        </div>
+      )}
+    </nav>
+  );
+};
 export default Navigation;
--- src/Components/Products/ProductCardLarge/ProductCardLarge.js
+++ src/Components/Products/ProductCardLarge/ProductCardLarge.js
@@ -1,40 +1,45 @@
-import React from 'react';
-import './ProductCardLarge.css';
-import productlarge from './productlarge.jpeg';
+import React from "react";
+import "./ProductCardLarge.css";
+import productlarge from "./productlarge.jpeg";
 
-
-
-const ProductCardLarge = ({updateInterestedBasket}) => {
-
+const ProductCardLarge = ({ updateInterestedBasket }) => {
   return (
-
-    <div className=''>
-    <article className="br2 ba dark-gray b--black-10 mv4 w-100 mw5 grow center shadow-5">
-      <img src={productlarge} className="db w-100 br2 br--top" alt="Photo of a kitten looking menacing." />
-      <div className="pa2 ph3-ns pb3-ns">
-        <div className="dt w-100 mt1">
-          <div className="dtc">
-            <h1 className="f5 f4-ns mv0">500l +</h1>
+    <div className="">
+      <article className="br2 ba dark-gray b--black-10 mv4 w-100 mw5 grow center shadow-5">
+        <img
+          src={productlarge}
+          className="db w-100 br2 br--top"
+          alt="Kitten looking menacing."
+        />
+        <div className="pa2 ph3-ns pb3-ns">
+          <div className="dt w-100 mt1">
+            <div className="dtc">
+              <h1 className="f5 f4-ns mv0">500l +</h1>
+            </div>
+            <div className="dtc tr">
+              <h2 className="f5 mv0">Contact for Pricing</h2>
+            </div>
           </div>
-          <div className="dtc tr">
-            <h2 className="f5 mv0">Contact for Pricing</h2>
-          </div>
-        </div>
-        <p className="f6 lh-copy measure mt2 mid-gray">
-         If you are interested in ordering large quantities of soil we do onsite mixing or pre-mixed deliveries depending on size. Contact for pricing!
+          <p className="f6 lh-copy measure mt2 mid-gray">
+            If you are interested in ordering large quantities of soil we do
+            onsite mixing or pre-mixed deliveries depending on size. Contact for
+            pricing!
           </p>
+        </div>
+      </article>
+      <div className="flex items-center mb2 centerPersonal b--black-10 shadow-5 pa3 m5 grow">
+        <input
+          className="mr2 myInput"
+          type="checkbox"
+          id="spacejam"
+          value="lrgSoil"
+          onClick={updateInterestedBasket}
+        />
+        <label htmlFor="spacejam" className="lh-copy">
+          I'm interested
+        </label>
       </div>
-    </article>
-    <div className="flex items-center mb2 centerPersonal b--black-10 shadow-5 pa3 m5 grow">
-      <input className="mr2 myInput" type="checkbox" id="spacejam" value="lrgSoil" onClick={updateInterestedBasket}/>
-      <label htmlFor="spacejam" className="lh-copy">I'm interested</label>
     </div>
-    </div>
- 
-
-
-  )
-
-
-}
+  );
+};
 export default ProductCardLarge;
